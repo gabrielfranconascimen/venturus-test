5b50833dead6e83f65d7164ccc1da869c9a593c9
Gabriel Franco Nascimento <gabriel.nascimento@dextra-sw.com>
2021-03-07 12:33:30 -0300

    Create component and add icons

---
+++ app/src/main/java/com/example/venturustest/components/HorizontalTextIconView.kt
@@ -0,0 +1,26 @@
+package com.example.venturustest.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.venturustest.R
+
+class HorizontalTextIconView constructor(context: Context, attrs: AttributeSet): ConstraintLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.component_horizontal_text_icon ,this)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.HorizontalTextIconView)
+        val icon = attributes.getResourceId(R.styleable.HorizontalTextIconView_icon, R.drawable.ic_launcher_foreground)
+        attributes.recycle()
+
+        findViewById<ImageView>(R.id.iv_icon).setImageResource(icon)
+    }
+
+    fun setText(text: String) {
+        findViewById<TextView>(R.id.tv_value).text = text
+    }
+
+}
