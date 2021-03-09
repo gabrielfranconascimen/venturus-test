02b401e1e7227506db03dfcc91a8416405c752b7
Gabriel Franco Nascimento <gabriel.nascimento@dextra-sw.com>
2021-03-08 21:54:00 -0300

    Create main screen

--- app/src/main/java/com/example/venturustest/MainActivity.kt
+++ app/src/main/java/com/example/venturustest/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.venturustest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.venturustest.presentation.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_start).setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
